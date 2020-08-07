b7fb0780588f5a2d0a08dc81cf564f809398b1dd
PabloLopez1998 <35881455+PabloLopez1998@users.noreply.github.com>
2020-08-06 21:41:34 -0700

    Initial commit

---
+++ app/src/main/java/pablo/myexample/gopray/MainActivity.kt
@@ -0,0 +1,11 @@
+package pablo.myexample.gopray
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
